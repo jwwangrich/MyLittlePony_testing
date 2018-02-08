49bad8b07794327479e617ac09841d36901c059e
Enoch Chang <enoch@Enochs-MacBook-Pro-3.local>
2018-02-07 22:42:59 -0500

    Create master branch

--- sum_num.py
+++
@@ -1,6 +0,0 @@
-def sum_numbers(num_list):
-   sum = 0
-   for num in num_list:
-      sum = sum + num
-      
-   return sum
--- test_sum_num.py
+++
@@ -1,9 +0,0 @@
-def test_sum_numbers():
-	from sum_num import sum_numbers
-	summed_output_1 = sum_numbers([1,5,8,3,6,3])
-	summed_output_2 = sum_numbers([-4,5,7,5,3,-5])
-	summed_output_3 = sum_numbers([0.5, -0.3, 0.2, -0.5])
-	assert summed_output_1 == 26
-	assert summed_output_2 == 11
-	assert summed_output_3 - 0.1 < 0.0001
-	
