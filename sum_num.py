7c6f295aa4b3700aa1c1ca1fd4f5492463817639
Enoch Chang <enoch@Enochs-MacBook-Pro-3.local>
2018-02-09 12:53:55 -0500

    Corrected pep8 for both files

--- sum_num.py
+++ sum_num.py
@@ -1,6 +1,5 @@
 def sum_numbers(num_list):
-   sum = 0
-   for num in num_list:
-      sum = sum + num
-      
-   return sum
+    sum = 0
+    for num in num_list:
+        sum = sum + num
+    return sum
