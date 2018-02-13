b709c787c7ac25d50227d502a9d33b7ca0c24f89
Wang Rich <wangrich@Wangde-MacBook-Pro.local>
2018-02-13 17:19:15 -0500

    revise_testmax

--- sum_num.py
+++ sum_num.py
@@ -4,14 +4,10 @@ def sum_numbers(num_list):
         sum = sum + num
     return sum
 
-
-
-
 def MaxDiff(num_list):
     max_diff = abs(num_list[1] - num_list[0])
     for i in range(0, num_list_size):
-        for j in range(i + 1, num_list_size):
+        for j in range(i + 1, num_list):
             if (abs(num_list[j] - num_list[i]) > max_diff):
                 max_diff = abs(num_list[j] - num_list[i])
-
     return max_diff
