ec6d9c7ff9b98e9270b6a727ca005ecb72a49529
Wang Rich <wangrich@Wangde-MacBook-Pro.local>
2018-02-13 18:09:44 -0500

    r_max

--- sum_num.py
+++ sum_num.py
@@ -7,8 +7,8 @@ def sum_numbers(num_list):
 
 def MaxDiff(num_list):
     max_diff = num_list[1] - num_list[0]
-    for i in range(0, num_list):
-        for j in range(i + 1, num_list):
+    for i in num_list:
+        for j+1 in num_list:
             if (num_list[j] - num_list[i] > max_diff):
                 max_diff = num_list[j] - num_list[i]
     return max_diff
