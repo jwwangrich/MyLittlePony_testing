77c28ba0a5086179653c7a71cd8ef8fc09a21a83
jwwangrich <jwwangrich@gmail.com>
2018-02-14 18:08:14 -0500

    changemaxdiff

--- sum_num.py
+++ sum_num.py
@@ -6,15 +6,42 @@ def sum_numbers(num_list):
 
 
 def MaxDiff(num_list):
-    max_diff = num_list[1] - num_list[0]
-    i = 0
-    j = i + 1
-    for i in num_list:
-        for j in num_list:
-            if (j-i > max_diff):
-                max_diff = j - i
-    return max_diff
+    """ returns the MaxDiff value
 
+    :param:  num_list: list of values
+    :param:  i : the index in num_list
+    :param:  j : the index i + 1 in num_list
+    :returns: return the max diff between two adjacent value from input num_list
+    :raises: ImportError
+    :raises: TypeError
+    :raises: ValueError
+    """
+
+    try:
+        import logging
+    except ImportError:
+        logging.error("No such file")
+        print("No Imported file")
+
+    logging.basicConfig(filename='divlog.txt', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
+    if len(num_list) == 0:
+        logging.warning('This is not a list')
+
+    try:
+        max_diff = num_list[1] - num_list[0]
+        i = 0
+        j = i + 1
+        for i in num_list:
+           for j in num_list:
+              if (j-i > max_diff):
+                  max_diff = j - i
+        return max_diff
+    except TypeError:
+        pass
+    except ValueError:
+        pass
+    logging.info('Status quo')
 
 def find_extremes(num_list):
     Minimum = min(num_list)
--- test_max_diff.py
+++ test_max_diff.py
@@ -1,8 +1,17 @@
 def test_max_diff():
     from sum_num import MaxDiff
+    import math
+    import pytest
     maxdiff_output_1 = MaxDiff([-5, 2, 6, 1, -7, 10])
     maxdiff_output_2 = MaxDiff([9, 8, -7, 6, 5, -4])
     maxdiff_output_3 = MaxDiff([0.5, -0.9, 0.1, -0.2])
+    maxdiff_output_4 = MaxDiff(['sdj', 5, 8, 10])
     assert maxdiff_output_1 == 17
     assert maxdiff_output_2 == 16
     assert maxdiff_output_3 - 1.4 < 0.0001
+    with pytest.raises(ValueError):
+        MaxDiff(math.sqrt(2), math.sqrt(-5), math.sqrt(8))
+    with pytest.raises(TypeError):
+        MaxDiff('9', 5, 9, 5, 9, 5)
+    with pytest.raises(ImportError):
+        import sum_num
